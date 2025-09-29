f045abbe9664e001cf19477ef1a5331988499183
AlvaroMercado07 <alvaromercado0207@gmail.com>
2025-09-29 16:38:51 -0500

    añadiendo las clases combo y hamburguesacombo

--- src/model/combo.java
+++ src/model/combo.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
-/**
- *
- * @author alvar
- */
-public class combo {
-    
-}
+
+public abstract class combo {
+   protected String nombre; 
+   public String getNombre() {
+        return nombre;
+   public String detalle(){ 
+            return nombre; 
+        }   
+    }
